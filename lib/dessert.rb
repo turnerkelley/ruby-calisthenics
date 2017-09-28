59416439347b9c85474f7a230f7c48036dde7b76
Turner Kelley <tkelley2@citadel.edu>
2017-09-28 03:19:32 +0000

    finished

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,37 @@
 class Dessert
+  attr_accessor :name
+  attr_accessor :calories
   def initialize(name, calories)
-    # your code here
+    @name=name
+    @calories=calories
   end
   def healthy?
-    # your code here
+   @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(flavor)
+  @flavor=flavor
+  @name=@flavor + ' jelly bean'
+  @calories=5
+  end
     # your code here
+  
+  def delicious?
+  if flavor == "licorice"
+    return false
+  else 
+    return true
+  end
+  end
+  def healthy?
+    if delicous == false
+      @calories = 5
+    end
   end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,22 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    s = self.downcase.gsub(/\W/i,'')
+    s == s.reverse
   end
+  
+  
   def count_words
-    # your code here
+    count = Hash.new(0)
+    word = self.downcase.gsub(/\b[a-zA-Z0-9]+/x)
+    word.each { |word| count[word] += 1}
+    return count
   end
+  
+  
+  
   def anagram_groups
-    # your code here
+    anagram = Hash.new(0)
+
   end
 end
 
